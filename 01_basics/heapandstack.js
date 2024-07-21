37824abc769834d283c13ef187e73b45cd75d0fd
Faizan Ali <52883063+f656@users.noreply.github.com>
2024-07-21 09:21:00 +0000

    heap and stack memory concept done

---
+++ 01_basics/heapandstack.js
@@ -0,0 +1,22 @@
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// stack(Primitive),  Heap(Non-Primitive)
+
+let myyoutubename = "faizanAlidotcom"
+
+let anothername = myyoutubename
+anothername = "chai aur code "
+
+console.log(myyoutubename);
+console.log(anothername);
+
+
+let userOne = {
+    email:"user@google.com",
+    upi: "user@ybl"
+}
+
+let usertwo = userOne
+usertwo.email = "faiznali@4958gmail.com"
+
+console.log(userOne.email);
+console.log(usertwo.email);
